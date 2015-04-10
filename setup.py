f1c2153edb718fee601f0bf40195807557feac6b
askeing <askeing@gmail.com>
2015-04-10 17:48:14 +0800

    try to update README.rst as long_description

--- setup.py
+++ setup.py
@@ -1,32 +1,28 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
 import os
 from setuptools import setup, find_packages
 
 # get documentation from the README
-here = os.path.dirname(os.path.abspath(__file__))
-try:
-    with open(os.path.join(here, 'README.rst')) as f:
-        description = f.read()
-except:
-    description = ''
+long_description = ''
+with open('README.rst') as doc:
+    long_description = doc.read()
 
 # version
-try:
-    with open('diff_py' + os.sep + 'VERSION') as f:
-        version = f.readline().strip()
-except:
-    version = '0.0.1'
+version = '0.0.1'
+with open('diff_py' + os.sep + 'VERSION') as f:
+    version = f.readline().strip()
 
 # dependencies
-try:
-    with open('diff_py' + os.sep + 'requirements.txt') as f:
-        deps = f.read().splitlines()
-except:
-    deps = []
+deps = []
+with open('diff_py' + os.sep + 'requirements.txt') as f:
+    deps = f.read().splitlines()
 
 setup(name='diff_py',
       version=version,
       description='The simple diff tool which is written by Python. The diff result can be printed in console or to html file.',
-      long_description=description,
+      long_description=long_description,
       classifiers=[],  # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
       keywords='diff html ',
       author='Askeing Yen',
